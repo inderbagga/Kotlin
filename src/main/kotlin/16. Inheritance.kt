222f071a469eafa4e8fa8a2a7b25293550e6dc02
Inder <er.inderbagga@gmail.com>
2021-05-06 00:16:25 +0530

    Updated Inheritance example case.

--- src/main/kotlin/16. Inheritance.kt
+++ src/main/kotlin/16. Inheritance.kt
@@ -1,7 +1,11 @@
 fun main(){
 
+    val animal=Animal("elephant",14)
     val cat=Cat("cat",4)
     cat.sound()
+
+    val math=Addition(1,2)
+    println("The sum is ${math.add()}")
 }
 
 open class Animal(type:String,age:Int){
@@ -25,4 +29,11 @@ class Cat(type:String, age:Int): Animal(type,age){
     override fun sound() {
         println("$name makes sound of meow.")
     }
+}
+
+class Addition(num1:Int,num2:Int):Math(num1,num2)
+
+open class Math(private val num1:Int, private val num2:Int){
+
+   open fun add()=num1.plus(num2)
 }
