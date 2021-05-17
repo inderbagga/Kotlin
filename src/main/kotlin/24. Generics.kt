ef012ba0171dbdd8085c0d0f3b99dc6636945ebc
Inder Bagga <er.inderbagga@gmail.com>
2021-05-17 22:52:00 +0530

    Added usecase of generic classes.

--- src/main/kotlin/24. Generics.kt
+++ src/main/kotlin/24. Generics.kt
@@ -1,3 +1,14 @@
 fun main(){
 
+    val objA=CustomType("String")
+    val objB=CustomType(123)
+    val objC=CustomType(listOf(1,2,3))
+}
+
+class CustomType<T>(value:T){
+    init{
+        println(" and length is ${value.toString().also { 
+            print("the value is $it")
+        }.length}")
+    }
 }
