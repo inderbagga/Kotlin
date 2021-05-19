db905dab51579909d0e8733df0cdbd4c57138bb6
Inder Bagga <er.inderbagga@gmail.com>
2021-05-19 13:19:55 +0530

    Added extension function usecase.

--- src/main/kotlin/27. ExtensionFunctions.kt
+++ src/main/kotlin/27. ExtensionFunctions.kt
@@ -1,3 +1,19 @@
 fun main(){
 
+    15.maths(6)
+    "Odd".midValue()
+    "Even".midValue()
+}
+
+fun Int.maths(num:Int){
+
+    println("Addition is: ${this+num}")
+    println("Subtraction is: ${this-num}")
+    println("Multiplication is: ${this*num}")
+}
+
+fun String.midValue(){
+
+    if(this.length%2!=0) println("$this's midpoint is at position: ${this.length/2}")
+    else println("String has no midpoint.")
 }
